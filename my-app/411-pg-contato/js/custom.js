971857d8b141d2f1cd08f05970a40d62fcb21869
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-08-21 15:17:13 -0300

    v1

--- my-app/411-pg-contato/js/custom.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#menu-btn').addEventListener('click', function() {
-    //console.log("Menu");
-
-    document.querySelector('#menu-site').classList.toggle("active");
-    document.querySelector('#menu-icon').classList.toggle("active");
-});
