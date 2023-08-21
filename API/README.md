be057d2ba1dd73303ec99e8d6931c0b47fb65cb7
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-08-21 15:13:17 -0300

    migrations

---
+++ API/README.md
@@ -0,0 +1,5 @@
+Criar migration da pagina home do conteudo do topo
+### npx sequelize-cli migration:generate --name creat-homes-tops
+
+Execultar migrtaions 
+### npx sequelize-cli db:migrate
---
+++ API/db/migrations/20230821175527-creat-homes-tops.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.createTable('Homestops', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER 
+      },
+      titleOneTop: {
+        type: Sequelize.STRING
+      },
+      titleTwoTop: {
+        type: Sequelize.STRING
+      },
+      titleThreeTop: {
+        type: Sequelize.STRING
+      },
+      linkBtnTop: {
+        type: Sequelize.STRING
+      },
+      textBtnTop: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+
+    }); 
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+     await queryInterface.dropTable('Homestops');
+     
+  }
+};
